fd7cc30c35597c760b46ba1c9b03d22f487f1eb3
alxia <aldridgexia@gmail.com>
2016-02-26 18:25:06 +0800

    First version.

---
+++ quantnet_tracker_spider.py
@@ -0,0 +1,30 @@
+import urllib
+import urllib2
+import re
+import os
+
+list_num = 0
+for page_num in range(1,11):
+	url = "https://www.quantnet.com/tracker/?page=" + str(page_num)
+	try:
+		request = urllib2.Request(url)
+		response = urllib2.urlopen(request)
+		content = response.read()
+		#r1 至r7 为分割后的正则表达式删选方法
+		r1 = '<div class="listBlock program">.*?<span class="programTitle"><a.*?>(.*?)</a>.*?<span class="type">(.*?)</span>.*?</div>'
+		r2 = '.*?<div class="listBlock ugpa">.*?<div class="titleText">(.*?)</div>'
+		r3 = '.*?<div class="listBlock GRE_Q">.*?<div class="titleText">(.*?)</div>'
+		r4 = '.*?<div class="listBlock GRE_V">.*?<div class="titleText">(.*?)</div>'
+		r5 = '.*?<div class="listBlock GRE_AWA">.*?<div class="titleText">(.*?)</div>'
+		r6 = '.*?<div class="listBlock submitted">.*?<div class="titleText">(.*?)</div>.*?</div>'
+		r7 = '.*?<div class="listBlock result">.*?<div class="titleText">.*?<span class="qnt_field_result_.*?">(.*?)</span>.*?<div class="secondRow">(.*?)</div>.*?'
+		
+		pattern = re.compile(r1+r2+r3+r4+r5+r6+r7,re.S)
+		items = re.findall(pattern,content)
+		tracker = [] #创建一个用于储存爬取结果的tracker 列表
+		for item in items:
+			list_num += 1
+			print list_num,item[0],item[1],item[2],item[3],item[4],item[5],item[6],item[7],item[8]
+			tracker.append(item)
+	except urllib2.URLError,e:
+		pass
